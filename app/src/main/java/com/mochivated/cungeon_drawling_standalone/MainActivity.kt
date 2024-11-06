aba23ccde7dab962677cfc8da6c8851193d79224
Suika Mochi <xodakat@live.de>
2024-11-06 10:02:59 +0100

    Update

--- app/src/main/java/com/mochivated/cungeon_drawling_standalone/MainActivity.kt
+++ app/src/main/java/com/mochivated/cungeon_drawling_standalone/MainActivity.kt
@@ -1,19 +1,23 @@
 package com.mochivated.cungeon_drawling_standalone
 
 import android.os.Bundle
-import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.mochivated.cungeon_drawling_standalone.src.entities.Player
 
 class MainActivity : AppCompatActivity() {
-    private var loadingImageView : ImageView? = null
+    private val player: Player = Player()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        loadingImageView = findViewById(R.id.loadingImageView)
+        
+        
+        
+        
+        
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
