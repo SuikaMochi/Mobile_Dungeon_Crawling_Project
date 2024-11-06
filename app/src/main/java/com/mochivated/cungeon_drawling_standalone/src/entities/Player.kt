68e54c3b0ea70edec0916d56da98d63c0cee9fb5
mochi <xodakat@live.de>
2024-11-07 00:01:09 +0100

    Update

--- app/src/main/java/com/mochivated/cungeon_drawling_standalone/src/entities/Player.kt
+++ app/src/main/java/com/mochivated/cungeon_drawling_standalone/src/entities/Player.kt
@@ -4,18 +4,31 @@ import android.content.Context
 import org.json.JSONObject
 
 class Player() : EntityBase() {
-	private var playerID: Int	= 0
+	private var playerID: Int		= 0
+	private var levelThreshold: Int	= 200
 	
 	private fun setPlayerID(id: Int)	{ playerID = id }
 	private fun getPlayerID(): Int		{ return playerID }
+
+	fun addExperience(exp: Int)			{
+		setEExperience(getEExperience() + exp)
+		if (getEExperience() >= levelThreshold)
+		{
+			setELevel(getELevel() + 1)
+			val leftover = getEExperience() - levelThreshold
+			setEExperience(leftover)
+			levelThreshold *= getELevel()
+		}
+	}
 	
 	fun loadPlayer(c: Context) {
-		val jsonSave = JSONObject(c.openFileInput("$playerID.sav").bufferedReader().readText())
+		val jsonSave = JSONObject(c.openFileInput("${getPlayerID()}.sav").bufferedReader().readText())
 		setPlayerID			(jsonSave.getInt("ID"))
 		
 		setEName			(jsonSave.getString("NAME"))
 		setEExperience		(jsonSave.getInt("EXPERIENCE"))
 		setELevel			(jsonSave.getInt("LEVEL"))
+		levelThreshold *= getELevel()
 		
 		setEStrength		(jsonSave.getInt("STRENGTH"))
 		setEEndurance		(jsonSave.getInt("ENDURANCE"))
@@ -26,5 +39,26 @@ class Player() : EntityBase() {
 		setEFaith			(jsonSave.getInt("FAITH"))
 		
 		loadInventory		(c, jsonSave.getJSONObject("INVENTORY"))
+	}
+
+	fun savePlayer(c: Context) {
+		val saveJson = """
+			{
+				"NAME": "${getEName()}",
+				"EXPERIENCE": ${getEExperience()},
+				"LEVEL": ${getELevel()},
+				"STRENGTH": ${getEStrength()},
+				"DEXTERITY": ${getEDexterity()},
+				"ENDURANCE": ${getEEndurance()},
+				"ATTUNEMENT": ${getEAttunement()},
+				"WISDOM": ${getEWisdom()},
+				"FAITH": ${getEFaith()},
+				"INVENTORY": 
+			}
+		""".trimIndent()
+
+		c.openFileOutput("${getPlayerID()}.sav", Context.MODE_PRIVATE).use {
+			it.write(saveJson.toByteArray())
+		}
 	}
 }
