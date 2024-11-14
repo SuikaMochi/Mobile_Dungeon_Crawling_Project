b5b93f5a0fa9b2b0bf4d0a45aa08a8bcaea56931
Suika Mochi <xodakat@live.de>
2024-11-14 15:41:31 +0100

    Update Lifted functions out for the different screen setups

--- app/src/main/java/com/mochivated/mobile_dungeon_crawling_project/MainActivity.kt
+++ app/src/main/java/com/mochivated/mobile_dungeon_crawling_project/MainActivity.kt
@@ -15,6 +15,11 @@ import kotlinx.coroutines.runBlocking
 class MainActivity : AppCompatActivity() {
 	private var player: Player? = null
 	private var openScreen: View? = null
+	private var loadingScreen: View? = null
+	private var failScreen: View? = null		//Used for debugging
+	private var successScreen: View? = null	//Used for debugging
+	private var characterCreatorScreen: View? = null
+	private var startScreen: View? = null
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
@@ -22,16 +27,54 @@ class MainActivity : AppCompatActivity() {
 		player = Player(applicationContext)
 		setContentView(R.layout.activity_main)
 		
-		val loadingScreen: View = findViewById(R.id.loading_screen)
-		val failScreen: View = findViewById(R.id.fail_screen)		//Used for debugging
-		val successScreen: View = findViewById(R.id.success_screen)	//Used for debugging
-		val characterCreatorScreen: View = findViewById(R.id.character_creator_screen)
-		val startScreen: View = findViewById(R.id.start_screen)
+		loadingScreen = findViewById(R.id.loading_screen)
+		failScreen = findViewById(R.id.fail_screen)		//Used for debugging
+		successScreen = findViewById(R.id.success_screen)	//Used for debugging
+		characterCreatorScreen = findViewById(R.id.character_creator_screen)
+		startScreen = findViewById(R.id.start_screen)
+		
+		setupStartScreen()
+		setupCharacterCreatorScreen()
+		
+		loadingScreen.run {
+			loadingScreen?.visibility = View.VISIBLE
+			openScreen = loadingScreen
+		}
 
-		val saveButton: Button = findViewById(R.id.saveButton)
-		val startButton: Button = findViewById(R.id.startButton)
+		switchTo(startScreen)
 
-		saveButton.setOnClickListener {
+		ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+			val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+			v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+			insets
+		}
+	}
+	
+	private fun switchTo(to: View?)
+	{
+		openScreen?.run { openScreen?.visibility = View.GONE }
+		to.run { to?.visibility = View.VISIBLE }
+		openScreen = to
+	}
+	
+	private fun setupStartScreen() {
+		findViewById<Button>(R.id.startButton)
+			.setOnClickListener {
+			switchTo(loadingScreen)
+			runBlocking {
+				try {
+					player?.loadPlayer()
+					switchTo(successScreen)
+				} catch (e: Exception) {
+					switchTo(characterCreatorScreen)
+					println(e.message)
+				}
+			}
+		}
+	}
+	
+	private fun setupCharacterCreatorScreen() {
+		findViewById<Button>(R.id.saveButton).setOnClickListener {
 			if (findViewById<EditText>(R.id.nameInput).text.isNotEmpty()) {
 				try {
 					player?.setEName(findViewById<EditText>(R.id.nameInput).text.toString())
@@ -50,38 +93,5 @@ class MainActivity : AppCompatActivity() {
 				}
 			}
 		}
-
-		startButton.setOnClickListener {
-			switchTo(loadingScreen)
-			runBlocking {
-				try {
-					player?.loadPlayer()
-					switchTo(successScreen)
-				} catch (e: Exception) {
-					switchTo(characterCreatorScreen)
-					println(e.message)
-				}
-			}
-		}
-
-		loadingScreen.run {
-			loadingScreen.visibility = View.VISIBLE
-			openScreen = loadingScreen
-		}
-
-		switchTo(startScreen)
-
-		ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-			val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-			v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-			insets
-		}
-	}
-	
-	private fun switchTo(to: View)
-	{
-		openScreen?.run { openScreen?.visibility = View.GONE }
-		to.run { to.visibility = View.VISIBLE }
-		openScreen = to
 	}
 }
